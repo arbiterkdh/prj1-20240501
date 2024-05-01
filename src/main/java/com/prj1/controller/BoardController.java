7ed6b7ddda5f47048ae4db6634bd4b2e925d583d
arbiterkdh <arbiterkdh@gmail.com>
2024-05-01 11:29:48 +0900

    게시판 작성

--- src/main/java/com/prj1/controller/BoardController.java
+++ src/main/java/com/prj1/controller/BoardController.java
@@ -4,8 +4,10 @@ import com.prj1.domain.Board;
 import com.prj1.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,11 +21,32 @@ public class BoardController {
     }
 
     @PostMapping("/add")
-    public String addPost(Board board) {
+    public String addPost(Board board, RedirectAttributes rttr) {
         System.out.println("board = " + board);
 
         service.add(board);
 
-        return null;
+        rttr.addAttribute("id", board.getId());
+        return "redirect:/board";
+    }
+
+    // /board?id=3
+    @GetMapping("/board")
+    public String view(Integer id, Model model) {
+        // 게시물 조회 (select)
+        Board board = service.get(id);
+        // 모델에 넣고
+        model.addAttribute("board", board);
+        // jsp 로 포워드
+        return "board/view";
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        // 게시물 목록 조회 (select)
+        // 모델에 넣고
+        model.addAttribute("boardList", service.list());
+        // jsp 로 포워드
+        return "board/home";
     }
 }
---
+++ src/main/webapp/WEB-INF/view/board/home.jsp
@@ -0,0 +1,36 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>Title</title>
+    <style>
+        table, tr, th, td {
+            border-collapse: collapse;
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+<h3>게시물 목록</h3>
+<table>
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>제목</th>
+        <th>작성자</th>
+        <th>작성일시</th>
+    </tr>
+    </thead>
+    <tbody>
+    <c:forEach items="${boardList}" var="board">
+        <tr>
+            <td>${board.id}</td>
+            <td>${board.title}</td>
+            <td>${board.writer}</td>
+            <td>${board.inserted}</td>
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
+</body>
+</html>
